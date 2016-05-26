b102917b26240c7a33575cc153d4220363a21ca0
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2016-05-26 17:04:35 +0200

    city dropdown

--- src/hello-world/my-input.component.ts
+++ src/hello-world/my-input.component.ts
@@ -1,4 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'city-dropdown',
+    template: `
+        <select (change)="cityChange.next($event.target.value)" value="{{city}}">
+            <option value=""></option>
+            <option value="Ankarsrum">Ankarsrum</option>
+            <option value="Västervik">Västervik</option>
+            <option value="Stockholm">Stockholm</option>
+        </select>
+    `
+})
+class CityComponent {
+    @Input() city;
+    @Output() cityChange = new EventEmitter();
+
+    onChange(event) {
+      this.cityChange.next(event.target.value);
+    }
+}
 
 @Component({
     selector: 'my-input',
@@ -9,14 +29,30 @@ import { Component } from '@angular/core';
         {{randomNumber}}
         {{username}}
         {{showUsername}}
+
+        <br>
+        <h2>City</h2>
+        <city-dropdown (cityChange)="cityChange($event)" city="Västervik"></city-dropdown>
+        {{city}}
+
+        <br>
+        <h2>local view variables</h2>
+        <br><h3>1</h3>
+        <input #myInput1 (keyup)='undefined'>
+        {{myInput1.value}}
+        <br><h3>2</h3>
+        <input #myInput2>
+        {{myInput2.value}}
+
     `,
-    directives: [],
+    directives: [CityComponent],
     providers: []
 })
 class MyInputComponent {
     username = 'Jesse';
     randomNumber = 5; // used a dice
     showUsername = '';
+    city = '';
 
     onClick(event) {
       this.randomNumber = Math.random() * 10;
@@ -24,6 +60,10 @@ class MyInputComponent {
 
     onShowClick(event) {
       this.showUsername = this.username;
+    }
+
+    cityChange(city) {
+      this.city = city;
     }
 }
 
